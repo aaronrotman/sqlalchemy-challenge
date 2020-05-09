95829c1740645c80862c4578a05fd7efa39f6177
Aaron Rotman <58377413+aaronrotman@users.noreply.github.com>
2020-05-08 21:33:49 -0700

    Complete precipitation and stations routes

--- app.py
+++ app.py
@@ -1,5 +1,25 @@
 # Dependencies  and set-up
-from flask import Flask
+import numpy as np
+import pandas as pd
+import datetime as dt
+from flask import Flask, jsonify
+# Python SQL toolkit and Object Relational Mapper
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, inspect
+
+# Database set-up
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+# Reflect the database into a new model
+Base = automap_base()
+
+# Reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Instantiate Flask
 app = Flask(__name__)
@@ -10,23 +30,79 @@ def index():
     print('Server received request for index page')
     return '''
     Welcome to the index page. \n
-    The following routes are available: \n
-    <a href="/about">About</a> \n
-    <a href="/contact">Contact</a>
+    The following routes are available:
+    <br>
+    <a href="/api/v1.0/precipitation">/api/v1.0/precipitation</a>
+    <br>
+    <a href="/api/v1.0/stations">/api/v1.0/stations</a>
+    <br>
+    <a href="/api/v1.0/tobs">/api/v1.0/tobs</a>
+    <br>
     '''
-# Create the about route
+# Create the precipitation route
 @app.route('/api/v1.0/precipitation')
-def about():
-    print('Server received request for about page')
-    return 'This is the about page <a href="../">Home</a>'
-# Create the contact route
-@app.route('/contact')
-def contact():
-    print('Server received request for contact page')
+def precipitation():
+    # Create session link to the database
+    session = Session(engine)
+    # Calculate the date one year before the last measurement date
+    year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    # Select all date and prcp values within one year of the most recent date
+    year_results = session.query(Measurement.date, Measurement.prcp).\
+    filter(Measurement.date > year_ago).\
+    order_by(Measurement.date).all()
+    
+    # Close the session
+    session.close()
+
+    year_data = []
+    for date, precip in year_results:
+        precip_dict = {}
+        precip_dict[date] = precip
+        # precip_dict['Date'] = date
+        # precip_dict['Precipitation'] = precip
+        year_data.append(precip_dict)
+        
+    print('Server received request for precipitation page')
+    return jsonify(year_data)
+    
+
+# Create the stations route
+@app.route('/api/v1.0/stations')
+def stations():
+     # Create session link to the database
+    session = Session(engine)
+
+    # Query database
+    station_results = session.query(Station.station).all()
+
+     # Close the session
+    session.close()
+
+    # Convert the results from list of tuples to list
+    all_stations = list(np.ravel(station_results))
+
+    print('Server received request for stations page')
+    return jsonify(all_stations)
+
+# Create the temperature observations route
+@app.route('/api/v1.0/tobs')
+def tobs():
+    print('Server received request for tobs page')
     return '''
-    This is the contact page \n
-    <a href="../">Home</a>
+    <a href="../../">Home</a>
     '''
+
+# # Create the start date route
+# @app.route('/api/v1.0/<start>')
+# def start_date():
+#     print('Server received request for start_date page')
+#     return '''
+#     <a href="../../"><Home</a>
+#     '''
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
