c2f18c10aed3c8a301c79228ce8f7400e0fbfc79
Aaron Rotman <58377413+aaronrotman@users.noreply.github.com>
2020-05-09 18:40:01 -0700

    Add dynamic routes with examples

--- app.py
+++ app.py
@@ -28,17 +28,33 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     print('Server received request for index page')
-    return '''
-    Welcome to the index page. \n
+    return (
+    '''
+    Welcome to the index page.
     The following routes are available:
+    <br><br>
+    STATIC ROUTES:
     <br>
     <a href="/api/v1.0/precipitation">/api/v1.0/precipitation</a>
     <br>
     <a href="/api/v1.0/stations">/api/v1.0/stations</a>
     <br>
     <a href="/api/v1.0/tobs">/api/v1.0/tobs</a>
+    <br><br>
+    DYNAMIC ROUTES:
     <br>
-    '''
+    Start date:
+    <br>
+    <a href="/api/v1.0/<start_date>">/api/v1.0/<start_date></a>
+    <br>
+    Example: <a href="/api/v1.0/2016-08-01">/api/v1.0/2016-08-01</a>
+    <br><br>
+    Start and end date:
+    <br>
+    <a href="/api/v1.0/<start_date>/<end_date>">/api/v1.0/<start_date>/<end_date></a>
+    <br>
+     Example: <a href="/api/v1.0/2016-08-01/2017-08-01">/api/v1.0/2016-08-01/2017-08-01</a>
+    ''')
 # Create the precipitation route
 @app.route('/api/v1.0/precipitation')
 def precipitation():
@@ -48,34 +64,30 @@ def precipitation():
     year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
     # Select all date and prcp values within one year of the most recent date
     year_results = session.query(Measurement.date, Measurement.prcp).\
-    filter(Measurement.date > year_ago).\
-    order_by(Measurement.date).all()
+        filter(Measurement.date > year_ago).\
+        order_by(Measurement.date).all()
     
     # Close the session
     session.close()
-
-    year_data = []
-    for date, precip in year_results:
-        precip_dict = {}
-        precip_dict[date] = precip
-        # precip_dict['Date'] = date
-        # precip_dict['Precipitation'] = precip
-        year_data.append(precip_dict)
-        
-    print('Server received request for precipitation page')
-    return jsonify(year_data)
     
+    # Create a dictionary from the query results
+    precip_dict = {}
+    for date, precip in year_results:
+        precip_dict[date] = precip
+       
+    print('Server received request for precipitation page')
+    return jsonify(precip_dict)
 
 # Create the stations route
 @app.route('/api/v1.0/stations')
 def stations():
-     # Create session link to the database
+    # Create session link to the database
     session = Session(engine)
 
     # Query database
     station_results = session.query(Station.station).all()
 
-     # Close the session
+    # Close the session
     session.close()
 
     # Convert the results from list of tuples to list
@@ -87,21 +99,69 @@ def stations():
 # Create the temperature observations route
 @app.route('/api/v1.0/tobs')
 def tobs():
+    # Create session link to the database
+    session = Session(engine)
+    # Calculate the date one year prior to the last temperature observation date
+    one_year_ago = dt.date(2017, 8, 18) - dt.timedelta(days=365)
+
+    # Query database
+    tobs_results = session.query(Measurement.date, Measurement.tobs).\
+        filter(Measurement.station=='USC00519281').\
+        filter(Measurement.date >= one_year_ago).\
+        order_by(Measurement.date).all()
+
+    # Close the session
+    session.close()
+
+    # Create a list of query results
+    tobs_list = []
+    
+    for date, tobs in tobs_results:
+        tobs_dict = {}
+        tobs_dict[date] = tobs
+        tobs_list.append(tobs_dict)
+
+
     print('Server received request for tobs page')
-    return '''
-    <a href="../../">Home</a>
-    '''
+    return jsonify(tobs_list)
+    
+# Create the start date route
+@app.route('/api/v1.0/<start_date>')
+def calc_temps_start(start_date):
+    # Create session link to the database
+    session = Session(engine)
+    
+    # Query the database
+    tobs_results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start_date).all()
+    
+    # Close the session
+    session.close()
+    """TMIN, TAVG, and TMAX for a list of dates.
+    
+    Args:
+        start_date (string): A date string in the format %Y-%m-%d
+        end_date (string): A date string in the format %Y-%m-%d
+        
+    Returns:
+        TMIN, TAVE, and TMAX
+    """
+    return jsonify(tobs_results)
 
-# # Create the start date route
-# @app.route('/api/v1.0/<start>')
-# def start_date():
-#     print('Server received request for start_date page')
-#     return '''
-#     <a href="../../"><Home</a>
-#     '''
-
-
-
+# Create the start/end date route
+@app.route('/api/v1.0/<start_date>/<end_date>')
+def calc_temps(start_date, end_date):
+    # Create session link to the database
+    session = Session(engine)
+    
+    # Query the database
+    tobs_results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
+    
+    # Close the session
+    session.close()
+ 
+    return jsonify(tobs_results)
 
 
 if __name__ == "__main__":
